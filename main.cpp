163ca80933411be54cc72f0fcfbf2a1353034bbc
Ben <benwilliams6299@gmail.com>
2021-03-21 00:36:35 +0000

    clear comments

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 int main()
 {
     // start here
-    //std::cout << "Qt version: "<< qVersion() << std::endl;
     control car;
     car.startCar();
     std::cout << "car started!\n";
@@ -16,26 +15,3 @@ int main()
     return 0;
 };
 
-/*
-
-    // define new Control object, car
-    control car;
-
-    car.startCar(0);
-
-
-    // commands as a test
-    car.timer();
-    car.shift("up");
-    car.shift("up");
-    car.timer();
-    car.shift("up");
-    car.shift("down");
-    car.shift("reverse");
-    car.shift("up");
-    car.shift("neutral");
-    car.timer();
-    car.shift("reverse");
-    car.timer();
-
-*/
